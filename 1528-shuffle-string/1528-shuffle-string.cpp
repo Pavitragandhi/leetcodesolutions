72c6c5933b2ec12d6423e774203e6b9e2c4833ae
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2023-10-27 00:50:45 +0530

    Time: 8 ms (47.53%), Space: 15.5 MB (42.50%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
         int n = indices.size();
-        string shuffled;
-        shuffled.resize(n , ' ');
+        string shuffled = s;
+        //shuffled.resize(n , ' ');
         
         for(int i = 0; i<n; i++){
             shuffled[indices[i]] = s[i];
