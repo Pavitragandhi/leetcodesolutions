4afa03c6173d9b07e6f2d3cbf9d33f30a98a469c
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2023-10-29 23:15:30 +0530

    Time: 22 ms (6.40%), Space: 39.1 MB (29.85%) - LeetHub

--- 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.cpp
+++ 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     bool isAcronym(vector<string>& words, string s) {
         string acro;
+        int i = 0;
         
         for(string letter : words){
+            if(letter[0] != s[i]){
+                return false;
+            }
             acro += letter[0];
+            i++;
         }
         
-        if(acro == s){
-            return true;
-        }
-        else{
+        if(acro.size() != s.size()){
             return false;
         }
+        
+        return true;
     }
 };
