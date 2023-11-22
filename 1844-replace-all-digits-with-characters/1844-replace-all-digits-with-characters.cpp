bcc4ace7151d5124e78c81ddfb1a05fef2671f9c
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2023-11-22 18:46:46 +0530

    Time: 3 ms (43.50%), Space: 6.5 MB (86.80%) - LeetHub

---
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string replaceDigits(string s) {
+        int n = s.size();
+        for(int i = 1; i<n; i+=2){
+            s[i] = shift(s[i-1] , s[i]);
+        }
+        return s;
+    }
+    
+private:
+    char shift(char c , char x){
+        int i = x - '0';
+        c += i;
+        return c;
+    }
+};
