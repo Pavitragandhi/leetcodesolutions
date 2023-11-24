4ea4e2ac7ad8fda073b9c8a4c6b8638ad62c0b5a
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2023-11-24 23:30:42 +0530

    Time: 14 ms (25.58%), Space: 9.7 MB (17.11%) - LeetHub

---
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeTrailingZeros(string num) {
+        int n = num.size();
+        string result = "";
+        
+        for(int i = n-1; i>=0; i--){
+            if(num[i] == '0' && result.empty()){
+                continue;
+            }
+            else{
+                result += num[i];
+            }
+        }
+        reverse(result.begin() , result.end());
+        return result;
+    }
+};
