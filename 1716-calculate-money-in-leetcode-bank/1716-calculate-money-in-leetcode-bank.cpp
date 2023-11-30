760436ead9cea194bb2dfaa6b39f0e03bfd06135
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2023-11-30 21:10:16 +0530

    Time: 2 ms (47.09%), Space: 6.4 MB (36.51%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        int count = 0;
+        int add = 0;
+        int sum = 0;
+        
+        for(int i = 1; i<=n; i++){
+            add++;
+            sum += add;
+            if(i%7 == 0){
+                count++;
+                add = count;
+            }
+        }
+        return sum;
+    }
+};
