175e4a86ae0676bd36833cd066456627285203d6
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2023-11-30 17:42:13 +0530

    Time: 39 ms (45.37%), Space: 6.2 MB (73.32%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countTriples(int n) {
+        int count = 0;
+        
+        for(int i = 1; i<n-1; i++){
+            int a = i*i;
+            for(int j = i+1; j<n; j++){
+                int b = j*j;
+                for(int k = j+1; k<n+1; k++){
+                    int c = k*k;
+                    if(c == a + b){
+                        count += 2;
+                    }
+                }
+            } 
+        }
+        return count;
+    }
+};
