6d7611e40bd86e099c79b12bb48c4ec2b9a2d186
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2023-12-03 16:28:58 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (15.07%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        while(num >= 10){
+            int sum = 0;
+            
+            while(num > 0){
+                int remainder = num%10;
+                sum += remainder;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+};
