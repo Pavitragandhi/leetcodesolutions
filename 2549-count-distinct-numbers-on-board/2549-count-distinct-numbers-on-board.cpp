82b03548f7fd4cdfb81b4a8864de7a9f0b739220
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2023-12-05 15:08:28 +0530

    Time: 5 ms (12.30%), Space: 6.8 MB (17.42%) - LeetHub

---
+++ 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int distinctIntegers(int n) {
+        set<int> unique_values;
+        unique_values.insert(n);
+        
+        while(n>2){
+            for(int i = 2; i<n; i++){
+                if(n%i == 1){
+                    unique_values.insert(i);
+                }
+            }
+            n--;
+        }
+        return unique_values.size();
+    }
+};
