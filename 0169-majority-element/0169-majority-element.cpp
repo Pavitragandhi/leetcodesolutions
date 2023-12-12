45dfb5fb0f7c5318aa0185e7585783e6961fd1b5
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2023-12-12 17:03:09 +0530

    Time: 14 ms (60.25%), Space: 20.3 MB (10.93%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int , int> majority_count;
+        int n = nums.size();
+        
+        for(int i = 0; i<n; i++){
+            majority_count[nums[i]]++;
+        }
+        
+        map<int , int , greater<int>> swap;
+        for(auto pair : majority_count){
+            swap[pair.second] = pair.first;
+        }
+        
+        int maximum = 0;
+        for(auto anwser : swap){
+            maximum = anwser.second;
+            break;
+        }
+        return maximum;
+    }
+};
