1fdc28378b45c5017b6555982c17a018511c8b4f
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2023-12-19 16:43:54 +0530

    Time: 15 ms (61.16%), Space: 27.8 MB (48.07%) - LeetHub

---
+++ 2032-two-out-of-three/2032-two-out-of-three.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
+        unordered_set<int> unique1(nums1.begin() , nums1.end());
+        unordered_set<int> unique2(nums2.begin() , nums2.end());
+        unordered_set<int> unique3(nums3.begin() , nums3.end());
+        
+        unordered_set<int> result;
+        
+        for(int number : unique1){
+            if(unique2.count(number) || unique3.count(number)){
+                result.insert(number);
+            }
+        }
+        
+        for(int number : unique2){
+            if(unique1.count(number) || unique3.count(number)){
+                result.insert(number);
+            }
+        }
+        
+        for(int number : unique3){
+            if(unique2.count(number) || unique1.count(number)){
+                result.insert(number);
+            }
+        }
+        vector<int> ans(result.begin() , result.end());
+        return ans;
+    }
+};
