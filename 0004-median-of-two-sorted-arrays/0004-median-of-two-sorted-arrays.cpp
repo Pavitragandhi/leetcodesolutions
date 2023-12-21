dda5bdcccb5badc5117fbfc43c31314fc45ef922
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2023-12-21 15:46:41 +0530

    Time: 37 ms (12.69%), Space: 89.6 MB (70.52%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,15 +4,15 @@ public:
         int m = nums1.size();
         int n = nums2.size();
         
-        auto findKthElement = [&](int k){
+        auto find_K_Element = [&](int k){
             int i = 0, j = 0;
 
             while (i < m || j < n){
                 if (i >= m){
-                    return nums2[j + k - 1];
+                    return nums2[j+k-1];
                 }
                 if (j >= n){
-                    return nums1[i + k - 1];
+                    return nums1[i+k-1];
                 }
 
                 int x = nums1[i];
@@ -23,24 +23,24 @@ public:
                 }
 
                 int p = k / 2;
-                int idx1 = std::min(i + p, m) - 1;
-                int idx2 = std::min(j + p, n) - 1;
+                int id_1 = std::min(i + p, m) - 1;
+                int id_2 = std::min(j + p, n) - 1;
 
-                if (nums1[idx1] < nums2[idx2]){
-                    k -= idx1 - i + 1;
-                    i = idx1 + 1;
+                if (nums1[id_1] < nums2[id_2]){
+                    k -= id_1 - i + 1;
+                    i = id_1 + 1;
                 } 
                 else{
-                    k -= idx2 - j + 1;
-                    j = idx2 + 1;
+                    k -= id_2 - j + 1;
+                    j = id_2 + 1;
                 }
             }
             return -1;
         };
 
        int total = m + n;
-       int mid1 = findKthElement((total + 1) / 2);
-       int mid2 = findKthElement((total + 2) / 2);
+       int mid1 = find_K_Element((total + 1) / 2);
+       int mid2 = find_K_Element((total + 2) / 2);
 
        return (mid1 + mid2) / 2.0;
     }
