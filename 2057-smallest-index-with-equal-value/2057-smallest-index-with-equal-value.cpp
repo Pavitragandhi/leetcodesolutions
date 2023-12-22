cc743fd24b659b19fd96a82158aa03fa7b60a50b
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2023-12-22 16:58:34 +0530

    Time: 14 ms (12.59%), Space: 22.2 MB (77.98%) - LeetHub

---
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int smallestEqual(vector<int>& nums) {
+        int n = nums.size();
+        int minimum = n;
+        bool check = false;
+        
+        for(int i = 0; i<n; i++){
+            if(i%10 == nums[i]){
+                check = true;
+                minimum = min(minimum , i);
+            }
+        }
+        if(check){
+            return minimum;
+        }
+        return -1;
+    }
+};
