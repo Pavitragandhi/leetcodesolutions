b3941227b064a7f0237b33f87e3e30ca0e53e8b0
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2024-01-06 15:05:10 +0530

    Time: 14 ms (21.18%), Space: 12 MB (5.06%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        multimap<int , int> store;
+        vector<int> result;
+        int n = arr.size();
+        sort(arr.begin() , arr.end());
+        
+        for(int i = 0; i<n; i++){
+            bitset<32> binaryconvert(arr[i]);
+            store.insert({binaryconvert.count() , arr[i]});
+        }
+        
+        for(auto num : store){
+            result.push_back(num.second);
+        }
+        
+        return result;
+    }
+};
