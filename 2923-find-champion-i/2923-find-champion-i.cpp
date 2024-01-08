41bfda03e8fbf606a2049ae4e6fb67a5bbae2d39
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2024-01-08 14:49:11 +0530

    Time: 71 ms (13.63%), Space: 38.4 MB (6.37%) - LeetHub

---
+++ 2923-find-champion-i/2923-find-champion-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findChampion(vector<vector<int>>& grid) {
+        int n = grid.size();
+        unordered_map<int , int> store;
+        
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                if(grid[i][j] == 1){
+                    store[i]++;
+                }
+            }
+        }
+        
+        map<int , int , greater<>> result;
+        for(auto pair : store){
+            result[pair.second] = pair.first;
+        }
+        auto it = result.begin();
+        return it->second;
+    }
+};
