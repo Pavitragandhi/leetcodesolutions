97f1318fcfa35ef6df8d08d6b61c4dc3c03a5eb9
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2024-01-09 20:02:58 +0530

    Time: 3 ms (50.94%), Space: 7.4 MB (23.11%) - LeetHub

---
+++ 0806-number-of-lines-to-write-string/0806-number-of-lines-to-write-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> numberOfLines(vector<int>& widths, string s) {
+        int lines = 1;
+        int size_of_line = 0;
+        
+        for(char c : s){
+            int char_width = widths[c - 'a'];
+            
+            if(size_of_line + char_width > 100){
+                lines++;
+                size_of_line = 0;
+            }
+            size_of_line += char_width;
+        }
+        return {lines , size_of_line};
+    }
+};
