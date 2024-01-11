e03e80ece2a197e942539e43b94837e622405ec5
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2024-01-11 14:23:24 +0530

    Time: 210 ms (67.77%), Space: 116.4 MB (69.87%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        int n = candyType.size()/2;
+        unordered_set<int> store(candyType.begin() , candyType.end());
+        int m = store.size();
+        
+        if(n >= m){
+            return m;
+        }
+        
+        return n;
+    }
+};
