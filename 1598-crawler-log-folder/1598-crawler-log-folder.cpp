f4e670eb6c8c4ff4d8f3a56c55f4ebaaa87b5442
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2024-01-19 14:04:10 +0530

    Time: 4 ms (77.84%), Space: 10.8 MB (62.08%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int result = 0;
+        
+        for(string s : logs){
+            if(s == "./"){
+                continue;
+            }
+            else if(s == "../"){
+                if(result == 0){
+                    continue;
+                }
+                else{
+                    result--;
+                }
+            }
+            else{
+                result++;
+            }
+        }
+        return result;
+    }
+};
