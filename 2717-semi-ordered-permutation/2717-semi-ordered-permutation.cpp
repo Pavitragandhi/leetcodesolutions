5f534bb742c7831eae230dd73bbc697fc0634e76
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2024-01-23 15:56:55 +0530

    Time: 21 ms (26.79%), Space: 50.1 MB (8.41%) - LeetHub

---
+++ 2717-semi-ordered-permutation/2717-semi-ordered-permutation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int semiOrderedPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int pos_1 , pos_2;
+        
+        for(int i = 0; i<n; i++){
+            if(nums[i] == 1){
+                pos_1 = i;
+            }
+            else if(nums[i] == n){
+                pos_2 = i;
+            }
+        }
+        
+        if(pos_1 < pos_2){
+            return pos_1 + (n-1 - pos_2);
+        }
+        else{
+            return pos_1 + (n-1 - pos_2) - 1;
+        }
+    }
+};
