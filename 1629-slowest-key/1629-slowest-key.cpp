183053537a6cc63ab8fc76a29b171f77274ba258
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2024-02-01 12:03:43 +0530

    Time: 5 ms (72.02%), Space: 13 MB (16.32%) - LeetHub

---
+++ 1629-slowest-key/1629-slowest-key.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char slowestKey(vector<int>& releaseTimes, string keysPressed) {
+        int n = releaseTimes.size();
+        int max_duration = releaseTimes[0];
+        char result = keysPressed[0];
+        
+        for(int i = 1; i<n; i++){
+            int duration = releaseTimes[i] - releaseTimes[i-1];
+            
+            if(duration > max_duration || (duration == max_duration && keysPressed[i] > result)){
+                result = keysPressed[i];
+                max_duration = duration;
+            }
+        }
+        return result;
+    }
+};
