e401e729cfa23c7a2465e28b6d4262b80f3160bd
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2024-02-25 15:55:24 +0530

    Time: 13 ms (28.79%), Space: 26.2 MB (42.63%) - LeetHub

---
+++ 2899-last-visited-integers/2899-last-visited-integers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> lastVisitedIntegers(vector<int>& nums) {
+        deque<int> seen;
+        vector<int> ans;
+        int k = 0;
+        int n = nums.size();
+        
+        for(int i = 0; i<n; i++){
+            if(nums[i] > 0){
+                seen.push_front(nums[i]);
+                k = 0;
+            }
+            else{
+                if(seen.size() > k){
+                    ans.push_back(seen[k]);
+                    k++;
+                }
+                else{
+                    ans.push_back(-1);
+                    k++;
+                }
+            }
+        }
+        return ans;
+    }
+};
