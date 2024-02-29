f05efa4d0b8dfe31a8b80c25396b1f43a8309051
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2024-02-29 18:44:40 +0530

    Time: 6 ms (9.87%), Space: 9.1 MB (10.75%) - LeetHub

---
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkAlmostEquivalent(string word1, string word2) {
+        int n = word1.size();
+        unordered_map<char , int> store_1;
+        unordered_map<char , int> store_2;
+        int check = 0;
+        
+        for(int i = 0; i<n; i++){
+            store_1[word1[i]]++;
+            store_2[word2[i]]++;
+        }
+        
+        for(auto pair : store_1){
+            check = abs(pair.second - store_2[pair.first]);
+            if(check > 3){
+                return false;
+            }
+        }
+        
+        for(auto pair : store_2){
+            check = abs(pair.second - store_1[pair.first]);
+            if(check > 3){
+                return false;
+            }
+        }
+        return true;
+    }
+};
