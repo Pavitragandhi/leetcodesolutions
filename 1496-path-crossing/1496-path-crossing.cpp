0e301c3556905e1ce4117ee30f86e0a6bb9f3be5
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2024-03-03 14:48:38 +0530

    Time: 6 ms (14.53%), Space: 8.3 MB (26.88%) - LeetHub

---
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isPathCrossing(string path) {
+        int x = 0;
+        int y = 0;
+        unordered_set<string> store;
+        store.insert("0,0");
+        
+        for(char c : path){
+            switch(c){
+                case 'N':
+                    y++;
+                    break;
+                    
+                case 'S':
+                    y--;
+                    break;
+                
+                case 'E':
+                    x++;
+                    break;
+                    
+                case 'W':
+                    x--;
+                    break;
+            }
+            
+            string position = to_string(x) + "," + to_string(y);
+            if(store.count(position) > 0){
+                return true;
+            }
+            
+            store.insert(position);
+        }
+        return false;
+    }
+};
