7435934a2ae503ac3a2280295e4d687822a67eff
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2024-03-05 11:32:12 +0530

    Time: 17 ms (48.83%), Space: 11.3 MB (10.08%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char , int> ran_store;
+        unordered_map<char , int> mag_store;
+        
+        if(ransomNote.size() > magazine.size()){
+            return false;
+        }
+        
+        for(char c : ransomNote){
+            ran_store[c]++;
+        }
+        
+        for(char c : magazine){
+            mag_store[c]++;
+        }
+        
+        for(auto pair : ran_store){
+            if(pair.second > mag_store[pair.first]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
