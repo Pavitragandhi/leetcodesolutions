f898773f1b0b1b0815a396635ce1e6b71949c15d
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2024-03-09 15:19:08 +0530

    Time: 3 ms (54.48%), Space: 9.9 MB (17.94%) - LeetHub

---
+++ 2451-odd-string-difference/2451-odd-string-difference.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string oddString(vector<string>& words) {
+        int n = words.size();
+        int m = words[0].size();
+        map<string , vector<int>> check_1;
+        map<vector<int> , int> check_2;
+        
+        for(int i = 0; i<n; i++){
+            vector<int> push;
+            for(int j = 0; j<m-1; j++){
+                int difference = words[i][j+1] - words[i][j];
+                push.push_back(difference);
+            }
+            check_1[words[i]] = push;
+            check_2[push]++;
+        }
+        
+        for(auto pair : check_1){
+            if(check_2[pair.second] == 1){
+                return pair.first;
+            }
+        }
+        return "-1";
+    }
+};
