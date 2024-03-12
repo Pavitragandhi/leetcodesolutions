096d3d0dda3329ec54af12ee6ff78315e9890cc8
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2024-03-12 22:11:43 +0530

    Time: 3 ms (40.83%), Space: 7.2 MB (88.58%) - LeetHub

---
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool strongPasswordCheckerII(string password) {
+        if(password.size() < 8){
+            return false;
+        }
+        
+        int n = password.size();
+        bool lower_case = false;
+        bool upper_case = false;
+        bool digit = false;
+        bool special = false;
+        
+        for(int i = 0; i<n; i++){
+            if(password[i] == password[i+1] && i != n-1){
+                return false;
+            }
+            
+            if(isdigit(password[i])){
+                digit = true;
+            }
+            else if(islower(password[i])){
+                lower_case = true;
+            }
+            else if(isupper(password[i])){
+                upper_case = true;
+            }
+            else{
+                special = true;
+            }
+        }
+        return (lower_case && upper_case && digit && special);
+    }
+};
