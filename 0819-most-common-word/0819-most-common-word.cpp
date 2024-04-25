9e2dce5aa31b1bd25e410849484273b77d9c820c
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2024-04-25 13:55:53 +0530

    Time: 8 ms (11.58%), Space: 9.9 MB (84.56%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string mostCommonWord(string paragraph, vector<string>& banned) {
+        string check = "";
+        for(char c : paragraph){
+            if(!isalpha(c)){
+                check += ' ';
+            }
+            else{
+                check += tolower(c);
+            }
+        }
+        
+        istringstream str(check);
+        string read;
+        unordered_map<string , int> store;
+        
+        while(str >> read){
+            if(find(banned.begin() , banned.end() , read) == banned.end()){
+                store[read]++;
+            }
+        }
+        
+        int maximum = 0;
+        string result = "";
+        
+        for(auto pair : store){
+            if(pair.second > maximum){
+                result = pair.first;
+                maximum = pair.second;
+            }
+        }
+        return result;
+    }
+};
