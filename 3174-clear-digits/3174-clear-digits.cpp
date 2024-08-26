091f2e998655997f92c3f8c2b81d4f2a42cf7322
Pavitragandhi <109463771+Pavitragandhi@users.noreply.github.com>
2024-08-26 19:01:37 +0530

    Time: 3 ms (37.45%), Space: 9 MB (15.21%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char> store;
+        
+        for(char c : s){
+            if(isalpha(c)){
+                store.push(c);
+            }
+            else{
+                store.pop();
+            }
+        }
+        
+        string result = "";
+        
+        while(!store.empty()){
+            char add = store.top();
+            store.pop();
+            
+            result = add + result;
+        }
+        
+        return result;
+    }
+};
